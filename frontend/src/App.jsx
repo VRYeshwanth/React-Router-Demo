a972d161ce02f39d220d9009af0b53beda52ac22
Yeshwanth <vryeshwanth06@gmail.com>
2025-10-02 17:26:06 +0530

    Navigation using <Link>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import Home from "./Home"
 import About from "./About"
+import Navbar from "./Navbar"
 import { Routes , Route } from "react-router-dom"
 
 export default function App() {
-    return <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/about" element={<About />}/>
-    </Routes>
+    return (
+        <div className="container">
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="/about" element={<About />}/>
+            </Routes>
+        </div>
+    )
 }
---
+++ frontend/src/Navbar.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+    return (
+        <nav>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+        </nav>
+    )
+}
