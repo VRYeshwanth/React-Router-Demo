afaa4f347338be13e729d8e7fa3c5ca0cfc5b605
Yeshwanth <vryeshwanth06@gmail.com>
2025-10-03 18:14:36 +0530

    Implement useNavigate()

--- frontend/src/About.jsx
+++ frontend/src/About.jsx
@@ -1,5 +1,11 @@
+import { useNavigate } from "react-router-dom";
+
 export default function About() {
+    const navigate = useNavigate();
     return (
-        <h1>About Page</h1>
+        <div className="about-page">
+            <h1>About Page</h1>
+            <button onClick={() => navigate("/")}>Go to Home</button>
+        </div>
     )
 }
